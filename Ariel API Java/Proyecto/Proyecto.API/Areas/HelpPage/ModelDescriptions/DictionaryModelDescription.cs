fe6e59c4f50cce7a0cdbc48e2dbef3f2f0b1445e
Paola Servis <paolaservis@yahoo.com.ar>
2016-08-04 19:59:40 -0300

    Eliminación del proyecto  duplicado.

--- Ariel API Java/Proyecto/Proyecto.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Proyecto.API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Ariel API Java/Proyecto/Proyecto.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Proyecto.API.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
