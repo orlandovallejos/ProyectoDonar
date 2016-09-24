5ce6dde35ae8b75684bdaa009b70e57a8bdbfd47
Orland <orlandovallejos@gmail.com>
2016-09-24 13:09:31 -0300

    now we can add files when they are selected

--- Proyecto/Proyecto.Web/app/app.js
+++ Proyecto/Proyecto.Web/app/app.js
@@ -4688,16 +4688,49 @@
 
 
 angular
-    .module('donarApp').directive('fileModel', ['$parse', function ($parse) {
+    .module('donarApp').directive('fileModel', ['$parse', '$http', function ($parse, $http) {
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
                 var model = $parse(attrs.fileModel);
                 var modelSetter = model.assign;
+                var carpeta = null;
+                if (attrs.fileCarpeta) {
+                    carpeta = attrs.fileCarpeta;
+                }
+
 
                 element.bind('change', function () {
                     scope.$apply(function () {
-                        modelSetter(scope, element[0].files[0]);
+                        var file = null;
+                        if (element[0].type && element[0].type === 'file') {
+                            modelSetter(scope, element[0].files[0]);
+                            file = element[0].files[0];
+                        }
+                        else {
+                            var fileInput = element[0].children[1];
+                            modelSetter(scope, fileInput.files[0]);
+                            file = fileInput.files[0];
+                        }
+
+                        if (carpeta && file) {
+                            var fd = new FormData();
+                            fd.append('file', file);
+                            fd.append('folder', carpeta);
+                            $http.post('../subir_imagen.php', fd, {
+                                transformRequest: angular.identity,
+                                headers: { 'Content-Type': undefined, 'Process-Data': false }
+                            })
+                                .success(function (response) {
+                                    console.log("Lista la imagen");
+                                    console.log(response);
+                                })
+                                .error(function (responseError) {
+                                    console.log("Error al listar la imagen");
+                                    console.log(responseError);
+                                });
+                        }
+
                     });
                 });
             }
